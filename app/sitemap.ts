1a80454528217d09d53ba21933c2811973394e02
kike-pinto <cripintort@gmail.com>
2026-05-17 02:07:28 -0400

    Add sitemap and robots configuration

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import type { MetadataRoute } from 'next'
+
+const baseUrl = 'https://reportingtoolsplatform.com'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/tools`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/tools/daily-report-generator`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/tools/shift-handover-generator`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/guides`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/guides/how-to-write-a-daily-report`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/guides/how-to-write-a-shift-handover-report`,
+      lastModified: new Date(),
+    },
+  ]
+}
